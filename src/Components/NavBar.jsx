4e186dc53e97b17d82fd5b209634cb228418a159
therealtmmy <oluwatimilehinsamson@gmail.com>
2023-10-03 15:23:17 +0100

    fixed the switch button

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,20 +1,37 @@
 import React from 'react'
 import "../Components/NavBar.css"
-import { FaMoon } from "react-icons/fa";
+import { useState } from 'react';
+import { FaMoon, FaRegLightbulb } from "react-icons/fa";
 
 const NavBar = () => {
-    const darkMode = () => {
-      console.log("Light")
-    }
+    const [showOn, setShowOn] = useState(false)
+
+    const toggleDarkMode = () => {
+      setShowOn(!showOn);
+      if (showOn) {
+        console.log('Switching to Light Mode');
+      } else {
+        console.log('Switching to Dark Mode');
+      }
+    };
 
   return (
     <>
       <div className='NavBar'>
         <p className='InTheWorld'>Where in the world?</p>
-
-        <div className='darkMode'>
-        <FaMoon /><button onClick={darkMode}>Dark Mode</button>
-        </div>
+      <div className='darkMode'>
+        {showOn ? (
+          <>
+            <FaRegLightbulb />
+            <button onClick={toggleDarkMode}>Light Mode</button>
+          </>
+        ) : (
+          <>
+            <FaMoon />
+            <button onClick={toggleDarkMode}>Dark Mode</button>
+          </>
+        )}
+      </div>
       </div>
     </>
   )
